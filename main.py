e86f4491729ee3b4779f69a341e9d985d6d345fb
Peery <peery.coding@gmail.com>
2021-10-28 12:48:26 +0200

    Reworked DataReader; Moved some Files
    Reworked DataReader to also support loading specific patients only
    Moved loading of the data sets to the first access

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-from DataLoader.DataReader import DataRead
+from IO.data_reader import DataReader
+
+import datetime
 
 
 if __name__ == '__main__':
-    readData = DataRead()
+    readData = DataReader()
+
+    start = datetime.datetime.now()
+    print("Starting to read one patient data:", str(start))
+
+    p = readData.get_patient_setA("p000095")
+
+    end = datetime.datetime.now()
+    print("Finished to read one patient data:", str(end))
+    print("Diff:", str(end-start))
+
+    print()
+    print("Starting to read training set A:", str(start))
+
+    training_setA = readData.training_setA
+
+    end = datetime.datetime.now()
+    print("Finished to read training set A:", str(end))
+    print("Diff:", str(end - start))
+
