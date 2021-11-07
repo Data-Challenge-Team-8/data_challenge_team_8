83d90972bbd64313824f653c996f3b41bbec9b16
Tobias Ettling <T.ETTLING@BC-R.org>
2021-11-07 17:07:00 +0100

    Changed the Layout for all tools, classed the tools, scaled the plot proportional to min max

---
+++ web/UI_tools/min_max_avg.py
@@ -0,0 +1,53 @@
+from IO.data_reader import DataReader
+from objects.training_set import TrainingSet
+from matplotlib import pyplot as plt
+import streamlit as st
+
+class MinMaxAvg:
+
+    def __init__(self, selected_tool: str, selected_label: str, selected_set: str, col3, col2, display: bool = True):
+        self.__training_set = {}
+        self.__selected_tool = selected_tool
+        self.__selected_label = selected_label
+        self.__selected_set = selected_set
+        self.__col3 = col3
+        self.__col2 = col2
+
+        if display:
+            max_label, min_label, avg_label = self.get_min_max_avg()
+            self.create_results(selected_label, max_label, min_label, avg_label)
+
+    def get_min_max_avg(self):
+        if not TrainingSet(
+                "exploratory_data_analysis_min_max_avg",
+                self.__training_set,
+                [self.__selected_tool, self.__selected_label, self.__selected_set]
+        ).is_cached():
+            dr = DataReader()
+            if self.__training_set == "Set A":
+                self.__training_set = dr.training_setA
+            elif self.__training_set == "Set B":
+                self.__training_set = dr.training_setB
+            else:
+                self.__training_set = dr.combined_training_set
+
+        analyse_tool = TrainingSet(
+            "exploratory_data_analysis_min_max_avg",
+            self.__training_set,
+            [self.__selected_tool, self.__selected_label, self.__selected_set]
+        )
+        max_label = analyse_tool.get_max_for_label(self.__selected_label)
+
+        min_label = analyse_tool.get_min_for_label(self.__selected_label)
+
+        avg_label = analyse_tool.get_avg_for_label(self.__selected_label)
+        return max_label, min_label, avg_label
+
+    def create_results(self, selected_label, max_label, min_label, avg_label):
+        fig, ax1 = plt.subplots()
+        ax1.bar(['max', 'min', 'average'], height=[float(max_label[1]), float(min_label[1]), avg_label], color="g")
+        ax1.set_title('Min, Max and average of ' + selected_label)
+        self.__col2.pyplot(fig)
+        self.__col3.metric("Max of " + selected_label, max_label[1])
+        self.__col3.metric("Min of " + selected_label, min_label[1])
+        self.__col3.metric("Average of " + selected_label, round(avg_label, 2))
--- web/categories/exploratory_data_analysis.py
+++ web/categories/exploratory_data_analysis.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 from IO.data_reader import DataReader
 from objects.training_set import TrainingSet
+from web.UI_tools.min_max_avg import MinMaxAvg
+from web.UI_tools.missing_values import MissingValues
 
 
 class ExploratoryDataAnalysis:
@@ -18,25 +20,25 @@ class ExploratoryDataAnalysis:
               "PTT", "WBC", "Fibrinogen", "Platelets", "age", "gender", "Unit1", "Unit2", "HospAdmTime", "ICULOS",
               "SepsisLabel"]
 
-    def __init__(self):
-
-        self.create_description()
+    def __init__(self, display: bool = True):
 
         self.__training_set = {}
 
-        tool_selected, set_selected, label_selected = self.create_selector_tools()
-        if tool_selected == 'Min, Max, Average':
-            self.create_min_max_avg(tool_selected, set_selected, label_selected)
-        if tool_selected == 'Missing Values':
-            self.create_missing_vals(tool_selected, label_selected, set_selected)
-        if tool_selected == 'Subgroups Groups':
-            pass
+        if display:
+            self.create_description()
+            col1, col2, col3 = st.columns((1, 2, 1))
+            tool_selected, set_selected, label_selected = self.create_selector_tools(col1)
+            if tool_selected == 'Min, Max, Average':
+                MinMaxAvg(tool_selected, label_selected, set_selected, col3, col2)
+            if tool_selected == 'Missing Values':
+                MissingValues(tool_selected, label_selected, set_selected, col3, col2)
+            if tool_selected == 'Subgroups Groups':
+                pass
 
     def create_description(self):
         st.write(self.DESCRIPTION)
 
-    def create_selector_tools(self):
-        col1, col2, col3 = st.columns(3)
+    def create_selector_tools(self, col1):
         selected_tool = col1.selectbox(
             'Choose a tool:',
             (
@@ -46,14 +48,14 @@ class ExploratoryDataAnalysis:
             )
         )
 
-        selected_label = col2.selectbox(
+        selected_label = col1.selectbox(
             'Choose a label:',
             (
                 self.LABELS
             )
         )
 
-        selected_set = col3.radio(
+        selected_set = col1.radio(
             "Choose your data",
             (
                 "Set A",
@@ -62,93 +64,3 @@ class ExploratoryDataAnalysis:
             )
         )
         return selected_tool, selected_set, selected_label
-
-    def create_missing_vals(self, selected_tool, selected_label, selected_set):
-        if TrainingSet(
-                "exploratory_data_analysis_missing_values",
-                self.__training_set,
-                [selected_tool, selected_label, selected_set]
-        ).is_cached():
-            missing_vals_rel = TrainingSet(
-                "exploratory_data_analysis_missing_values",
-                self.__training_set,
-                [selected_tool, selected_label, selected_set]
-            ).get_avg_rel_NaN_amount_for_label(selected_label)
-        else:
-            dr = DataReader()
-            if selected_set == "Set A":
-                self.__training_set = dr.training_setA
-            elif selected_set == "Set B":
-                self.__training_set = dr.training_setB
-            else:
-                self.__training_set = dr.combined_training_set
-
-            missing_vals_rel = TrainingSet(
-                "exploratory_data_analysis_missing_values",
-                self.__training_set,
-                [selected_tool, selected_label, selected_set]
-            ).get_avg_rel_NaN_amount_for_label(selected_label)
-
-        fig, ax = plt.subplots()
-        mylabels = [str(round((missing_vals_rel * 100), 2)) + "% NaN Values",
-                    str(round((1 - missing_vals_rel) * 100, 2)) + "% defined values"]
-        ax.pie([missing_vals_rel, 1 - missing_vals_rel], labels=mylabels, explode=[0.2, 0], colors=['r', 'g'])
-        st.pyplot(fig)
-
-    def create_min_max_avg(self, selected_tool, selected_set, selected_label):
-        if TrainingSet(
-                "exploratory_data_analysis_min_max_avg",
-                self.__training_set,
-                [selected_tool,
-                 selected_label,
-                 selected_set]
-        ).is_cached():
-
-            max_label = TrainingSet(
-                "exploratory_data_analysis_min_max_avg",
-                self.__training_set,
-                [selected_tool, selected_label, selected_set]
-            ).get_max_for_label(selected_label)
-
-            min_label = TrainingSet(
-                "exploratory_data_analysis_min_max_avg",
-                self.__training_set,
-                [selected_tool, selected_label, selected_set]
-            ).get_min_for_label(selected_label)
-
-            avg_label = TrainingSet(
-                "exploratory_data_analysis_min_max_avg",
-                self.__training_set,
-                [selected_tool, selected_label, selected_set]
-            ).get_avg_for_label(selected_label)
-        else:
-            dr = DataReader()
-            if selected_set == "Set A":
-                self.__training_set = dr.training_setA
-            elif selected_set == "Set B":
-                self.__training_set = dr.training_setB
-            else:
-                self.__training_set = dr.combined_training_set
-
-            max_label = TrainingSet(
-                "exploratory_data_analysis_min_max_avg",
-                self.__training_set,
-                [selected_tool, selected_label, selected_set]
-            ).get_max_for_label(selected_label)
-
-            min_label = TrainingSet(
-                "exploratory_data_analysis_min_max_avg",
-                self.__training_set,
-                [selected_tool, selected_label, selected_set]
-            ).get_min_for_label(selected_label)
-
-            avg_label = TrainingSet(
-                "exploratory_data_analysis_min_max_avg",
-                self.__training_set,
-                [selected_tool, selected_label, selected_set]
-            ).get_avg_for_label(selected_label)
-
-        col1, col2, col3 = st.columns(3)
-        col1.metric("Max of " + selected_label, max_label[1])
-        col2.metric("Min of " + selected_label, min_label[1])
-        col3.metric("Average of " + selected_label, round(float(avg_label), 2))
