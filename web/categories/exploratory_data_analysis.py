5ab2c30709fbaef7ffb889e5503eef757bfb3e5a
Tobias Ettling <T.ETTLING@BC-R.org>
2021-11-07 18:19:07 +0100

    Added data loader

--- web/categories/exploratory_data_analysis.py
+++ web/categories/exploratory_data_analysis.py
@@ -17,7 +17,7 @@ class ExploratoryDataAnalysis:
     LABELS = ["HR", "O2Sat", "Temp", "SBP", "MAP", "DBP", "Resp", "EtCO2", "BaseExcess", "FiO2", "pH", "PaCO2", "SaO2",
               "AST", "BUN", "Alkalinephos", "Calcium", "Chloride", "Creatinine", "Bilirubin_direct", "Glucose",
               "Lactate", "Magnesium", "Phosphate", "Potassium", "Bilirubin_total", "TroponinI", "Hct", "Hgb",
-              "PTT", "WBC", "Fibrinogen", "Platelets", "age", "gender", "Unit1", "Unit2", "HospAdmTime", "ICULOS",
+              "PTT", "WBC", "Fibrinogen", "Platelets", "Age", "Gender", "Unit1", "Unit2", "HospAdmTime", "ICULOS",
               "SepsisLabel"]
 
     def __init__(self, display: bool = True):
