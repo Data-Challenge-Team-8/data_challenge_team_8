0c52c2171c065ec8a3567576f92f0458667ac20f
Peery <peery.coding@gmail.com>
2021-11-22 11:28:24 +0100

    Fixed missing Label in Patient.LABELS
    HCO3 was missing in Patient.LABELS

--- objects/patient.py
+++ objects/patient.py
@@ -15,8 +15,8 @@ class Patient:
     Important: each row will represent a single hour's worth of data
     """
 
-    LABELS = ["HR", "O2Sat", "Temp", "SBP", "MAP", "DBP", "Resp", "EtCO2", "BaseExcess", "FiO2", "pH", "PaCO2", "SaO2",
-              "AST", "BUN", "Alkalinephos", "Calcium", "Chloride", "Creatinine", "Bilirubin_direct", "Glucose",
+    LABELS = ["HR", "O2Sat", "Temp", "SBP", "MAP", "DBP", "Resp", "EtCO2", "BaseExcess", "HCO3", "FiO2", "pH", "PaCO2",
+              "SaO2", "AST", "BUN", "Alkalinephos", "Calcium", "Chloride", "Creatinine", "Bilirubin_direct", "Glucose",
               "Lactate", "Magnesium", "Phosphate", "Potassium", "Bilirubin_total", "TroponinI", "Hct", "Hgb",
               "PTT", "WBC", "Fibrinogen", "Platelets", "Age", "Gender", "Unit1", "Unit2", "HospAdmTime", "ICULOS",
               "SepsisLabel"]
