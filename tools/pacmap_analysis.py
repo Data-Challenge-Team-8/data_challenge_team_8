a15328f99f355ce3d76eafaf7332d984288bdeff
Peery <peery.coding@gmail.com>
2021-11-22 22:37:07 +0100

    First PacMAP implementation
    Implemented methods to calculate averages in the TrainingSet class (get_average_df, get_label_averages).
    Removed class from pacmap_analysis.py and changed it to functions.
    Moved calculation and formatting into calculate_pacmap() so that it only takes a TrainingSet instance now.
    Decoupled plotting function from the calculation.

--- tools/pacmap_analysis.py
+++ tools/pacmap_analysis.py
@@ -1,29 +1,38 @@
+from objects.training_set import TrainingSet
+
 import pacmap
 from matplotlib import pyplot as plt
 
 
-# TODO: Import training set, transform to dataframe, dimensionality reduction -> return new dataframe, plot the new df
-class PacmapAnalysis:
+def calculate_pacmap(training_set: TrainingSet):
+    """
+    Calculate a PaCMAP transformation of the given TrainingSet.
 
-    def __init__(self, training_set):
-        self.training_set = training_set(selected_label='none',
-                                         selected_tool='none',
-                                         selected_set='Set A')
-        self.calculate_pacmap()
+    Based on TrainingSet.get_average_df() and the fix_missing_values flag
+    :param training_set:
+    :return: data as returned by the PaCMAP algorithm
+    """
+    avg_df = training_set.get_average_df(fix_missing_values=True)
+    avg_np = avg_df.transpose().to_numpy()
 
-    @classmethod
-    def get_pacmap_analysis(cls, training_set):
-        # if cached: return cached analysis-values ?
-        # else:
-        new_analysis = PacmapAnalysis(training_set)
-        return new_analysis
+    avg_np.reshape(avg_np.shape[0], -1)  # does this have an effect?
 
-    def calculate_pacmap(self):
-        old_dataframe = self.training_set.transform_into_df()
-        embedding = pacmap.PaCMAP(n_dims=3, n_neighbors=None, MN_ratio=0.5, FP_ratio=2.0)
-        X_transformed = embedding.fit_transform(old_dataframe.values, init="pca")
+    embedding = pacmap.PaCMAP(n_dims=2, n_neighbors=None, MN_ratio=0.5, FP_ratio=2.0, verbose=True)
+    data_transformed = embedding.fit_transform(avg_np, init="pca")
 
-        self.plot_pacmap(X_transformed)
+    return data_transformed
 
-    def plot_pacmap(self, new_dataframe):
-        pass
+
+def plot_pacmap(data):
+    """
+    Plots the given PaCMAP data using matplotlib
+    :param data:
+    :return:
+    """
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1)
+
+    ax1.scatter(data[:, 0], data[:, 1], cmap="Spectral", c="r", s=0.6)
+
+    plt.show()
+    plt.close()
