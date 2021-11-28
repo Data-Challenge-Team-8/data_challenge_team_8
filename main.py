88adc9b913c33955b13690b61cf44f6d85199e96
Peery <peery.coding@gmail.com>
2021-11-28 16:30:50 +0100

    Added more cluster plots, Silhouette Score
    Silhouette Score is now calculated along with kmeans clustering.
    Added an additional plotting method to more easily spot cluster differences based on sepsis

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from objects.training_set import TrainingSet
 from tools.visualization.interpolation_comparison import plot_most_interesting_interpolation_patients, \
     plot_data_with_and_without_interpolation
-from tools.pacmap_analysis import calculate_pacmap, plot_pacmap2D_sepsis, plot_pacmap2D
-from tools.clustering_analysis import calculate_cluster_dbscan, calculate_cluster_kmeans
+from tools.pacmap_analysis import calculate_pacmap
+from tools.clustering_analysis import calculate_cluster_dbscan, calculate_cluster_kmeans, calculate_silhouette_score, \
+    plot_clustering_with_silhouette_score_sepsis
 
 
 if __name__ == '__main__':
@@ -15,14 +16,18 @@ if __name__ == '__main__':
     # Pacmap for Sepsis
     # TODO: Können wir das Pacmap data cachen? Dann gehen die ganzen Clusterings viel schneller.
     data, patient_ids = calculate_pacmap(set_a)
-    # plot_pacmap2D_sepsis(f"PaCMAP colored by sepsis ({set_a.name})", data, patient_ids, training_set=set_a)
 
     # Calculate different clusters
     # TODO: Wir brauchen eigentlich standardisierte Daten: z = (wert_i - mean) / std_dev
-    # TODO: DBSCAN funktioniert gar nicht??
     # db_scan_list = calculate_cluster_dbscan(set_a, eps=0.05, min_samples=200)
     # plot_pacmap2D(f"PaCMAP colored by DBSCAN clusters ({set_a.name})", data, coloring=db_scan_list, color_map='tab20c', save_to_file=True)
+
     amount_of_clusters = 12
-    k_means_list = calculate_cluster_kmeans(set_a, n_clusters=amount_of_clusters)
-    title = "PaCMAP colored by " + str(amount_of_clusters) + " k-Means clusters " + str(set_a.name)
-    plot_pacmap2D(title, data, coloring=k_means_list, color_map='tab20c', save_to_file=True)
+    k_means_list, sh_score = calculate_cluster_kmeans(set_a, n_clusters=amount_of_clusters)
+    title = f"{amount_of_clusters} k-Means clusters ({set_a.name})"
+
+    plot_clustering_with_silhouette_score_sepsis(title, data, sh_score=sh_score, coloring=k_means_list,
+                                                 patient_ids=patient_ids, training_set=set_a,
+                                                 color_map='tab20c', save_to_file=True)
+
+
