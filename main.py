c292f7791f8fb1a9937869fd3469b91a75d26ba7
Peery <peery.coding@gmail.com>
2021-11-29 14:38:47 +0100

    Changed main.py
    Changed main.py to an up-to-date example of the newest functions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tools.visualization.interpolation_comparison import plot_most_interesting_i
     plot_data_with_and_without_interpolation
 from tools.pacmap_analysis import calculate_pacmap
 from tools.clustering_analysis import calculate_cluster_dbscan, calculate_cluster_kmeans, calculate_silhouette_score, \
-    plot_clustering_with_silhouette_score_sepsis
+    plot_clustering_with_silhouette_score_sepsis, training_set_to_data
 
 
 if __name__ == '__main__':
@@ -23,10 +23,26 @@ if __name__ == '__main__':
     # plot_pacmap2D(f"PaCMAP colored by DBSCAN clusters ({set_a.name})", data, coloring=db_scan_list, color_map='tab20c', save_to_file=True)
 
     amount_of_clusters = 12
-    k_means_list, sh_score = calculate_cluster_kmeans(set_a, n_clusters=amount_of_clusters)
+    k_means_list, sh_score = calculate_cluster_kmeans(training_set_to_data(set_a), n_clusters=amount_of_clusters)
     title = f"{amount_of_clusters} k-Means clusters ({set_a.name})"
 
     plot_clustering_with_silhouette_score_sepsis(title, data, sh_score=sh_score, coloring=k_means_list,
+                                                 patient_ids=patient_ids, training_set=set_a,
+                                                 color_map='tab20c', save_to_file=True)
+
+    # k-Means AFTER PaCMAP
+    k_means_list, sh_score = calculate_cluster_kmeans(data, n_clusters=amount_of_clusters)
+    title = f"{amount_of_clusters} k-Means clusters ({set_a.name}) after PaCMAP"
+    plot_clustering_with_silhouette_score_sepsis(title, data, sh_score=sh_score, coloring=k_means_list,
+                                                 patient_ids=patient_ids, training_set=set_a,
+                                                 color_map='tab20c', save_to_file=True)
+
+    #k-means with imputation
+    data_imp, patient_ids = calculate_pacmap(set_a, use_interpolation=True)
+
+    k_means_list, sh_score = calculate_cluster_kmeans(training_set_to_data(set_a, use_interpolation=True), n_clusters=amount_of_clusters)
+    title = f"{amount_of_clusters} k-Means ({set_a.name}) (interpolated)"
+    plot_clustering_with_silhouette_score_sepsis(title, data_imp, sh_score=sh_score, coloring=k_means_list,
                                                  patient_ids=patient_ids, training_set=set_a,
                                                  color_map='tab20c', save_to_file=True)
 
