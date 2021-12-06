6d9da2757b1d39a4d7ba012bff4a611f3e858161
Peery <peery.coding@gmail.com>
2021-12-06 17:30:12 +0100

    Biclustering Plotting
    Added visually appealing plotting of biclustering with the plot_bisclustering() method.

--- tools/subspace_clustering_analysis.py
+++ tools/subspace_clustering_analysis.py
@@ -1,30 +1,62 @@
-from typing import List
+from typing import Tuple
+import math
 import os
 
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import sklearn.base
 from sklearn import cluster
 from sklearn.cluster import DBSCAN, KMeans
-from sklearn.metrics import silhouette_score
+from sklearn.metrics import consensus_score
 
 from objects.training_set import TrainingSet
 from IO.data_reader import FIGURE_OUTPUT_FOLDER
-from tools.pacmap_analysis import plot_pacmap2D, calculate_pacmap
-
-def implement_bicluster(training_set):
-    calculate_bicluster(training_set)
 
 
-def calculate_bicluster(training_set):
+def implement_bicluster(training_set: TrainingSet, save_to_file: bool = False):
+    data, model = calculate_bicluster(training_set)
+    plot_biclustering(data, model, training_set, save_to_file=save_to_file)
+
+
+def calculate_bicluster(training_set: TrainingSet) -> Tuple[np.ndarray, cluster.SpectralCoclustering]:
     avg_np = training_set_to_data(training_set)
     X = avg_np
-    model = cluster.SpectralCoclustering(n_clusters=2)          # 2 = bicluster?
+    model = cluster.SpectralCoclustering(n_clusters=3, random_state=0)          # 2 = bicluster?
     model.fit(X)
-    fit_X = X[np.argsort(model.row_labels_)]
-    fit_X = fit_X[:, np.argsort(model.column_labels_)]
 
-    plt.matshow(fit_X, cmap="cool")
+    return X, model
+
+
+def plot_biclustering(data_raw: np.ndarray, model: sklearn.base.BiclusterMixin, training_set: TrainingSet,
+                      save_to_file: bool = False):
+    """
+    Sorts the data according to the biclusters and then plots the biclustering.
+    :param data_raw: the unsorted data to be plotted
+    :param model: Biclustering model used for getting the bicluster data
+    :param training_set: training_set that contained the data for the set name
+    :param save_to_file:
+    :return:
+    """
+    data_row = data_raw[np.argsort(model.row_labels_)]
+    data = data_row[:, np.argsort(model.column_labels_)]
+
+    fig = plt.figure()
+    fig.set_figheight(4)
+    fig.set_figwidth(4)
+    ax = fig.add_subplot()
+
+    ax.matshow(data, cmap=plt.cm.Blues)
+    ax.set_aspect(len(data[0]) / len(data))
+    ax.set_xlabel('Features')
+    ax.set_ylabel('Patients')
+    ax.set_title(f'"{training_set.name}" after biclustering; rearranged to show biclusters')
+
+    if not save_to_file:
+        plt.show()
+    else:
+        plt.savefig(os.path.join(f"bicluster-{training_set.name}"))
+    plt.close()
 
 
 def training_set_to_data(training_set: TrainingSet, use_interpolation: bool = False) -> np.ndarray:
