a8f253a921fdcda5558a5459fbac1dd911541b5c
Peery <peery.coding@gmail.com>
2021-12-06 19:03:10 +0100

    Implemented SpectralBiclustering
    Added SpectralBiclustering as an alternative to subspace_clustering_analysis.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import pickle
 
 from objects.training_set import TrainingSet
-from tools.subspace_clustering_analysis import implement_bicluster
+from tools.subspace_clustering_analysis import implement_bicluster_spectralcoclustering, \
+    implement_bicluster_spectralbiclustering
 from tools.visualization.interpolation_comparison import plot_most_interesting_interpolation_patients, \
     plot_data_with_and_without_interpolation
 from tools.pacmap_analysis import get_pacmap
@@ -13,16 +14,18 @@ if __name__ == '__main__':
     # ...
 
     # Task 2: Load Trainingset with interpolation (and caching)
-    # mini_set = TrainingSet(TrainingSet.PRESETS["Set A"][:61], name="Mini Set")
+    # set_a = TrainingSet(TrainingSet.PRESETS["Set A"][:61], name="Mini Set")
     set_a = TrainingSet.get_training_set("Set A")
     # plot_most_interesting_interpolation_patients(set_a)
 
     # Task 3: Implement different clustering methods
     pacmap_data, patient_ids = get_pacmap(set_a)
-    implement_DBSCAN(set_a, pacmap_data, patient_ids)
+    #implement_DBSCAN(set_a, pacmap_data, patient_ids)
     # implement_k_means(set_a, pacmap_data, patient_ids)
 
     # Task 4: Implement different subspace clustering methods
     # scikit_learn module biclustering, alternatives: houghnet, biclustlib
-    print("Implementing Biclustering: ")
-    implement_bicluster(set_a)
+    print("Doing Biclustering ...")
+    implement_bicluster_spectralcoclustering(set_a, use_interpolation=False)
+    # implement_bicluster_spectralbiclustering(set_a, use_interpolation=False)
+    # TODO convert result into cluster data for pacmap visualization?
--- tools/subspace_clustering_analysis.py
+++ tools/subspace_clustering_analysis.py
@@ -5,6 +5,7 @@ import os
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import sklearn.base
 from sklearn import cluster
 from sklearn.cluster import DBSCAN, KMeans
@@ -14,22 +15,43 @@ from objects.training_set import TrainingSet
 from IO.data_reader import FIGURE_OUTPUT_FOLDER
 
 
-def implement_bicluster(training_set: TrainingSet, save_to_file: bool = False):
-    data, model = calculate_bicluster(training_set)
-    plot_biclustering(data, model, training_set, save_to_file=save_to_file)
+def implement_bicluster_spectralcoclustering(training_set: TrainingSet, save_to_file: bool = False, use_interpolation: bool = False):
+    data, model, avg_df = calculate_bicluster_spectralcoclustering(training_set, use_interpolation=use_interpolation)
+    plot_biclustering(data, model, training_set, save_to_file=save_to_file, feature_names=avg_df.index.to_numpy(),
+                      used_interpolation=use_interpolation)
+
+    return model.row_labels_, model.column_labels_
 
 
-def calculate_bicluster(training_set: TrainingSet) -> Tuple[np.ndarray, cluster.SpectralCoclustering]:
-    avg_np = training_set_to_data(training_set)
+def implement_bicluster_spectralbiclustering(training_set: TrainingSet, save_to_file: bool = False,
+                                             use_interpolation: bool = False):
+    data, model, avg_df = calculate_bicluster_spectralbiclustering(training_set, use_interpolation=use_interpolation)
+    plot_biclustering(data, model, training_set, save_to_file=save_to_file, feature_names=avg_df.index.to_numpy(),
+                      used_interpolation=use_interpolation)
+
+    return model.row_labels_, model.column_labels_
+
+
+def calculate_bicluster_spectralcoclustering(training_set: TrainingSet, use_interpolation: bool = False) -> Tuple[np.ndarray, cluster.SpectralCoclustering, pd.DataFrame]:
+    avg_np, avg_df = training_set_to_data(training_set, use_interpolation=use_interpolation)
     X = avg_np
-    model = cluster.SpectralCoclustering(n_clusters=3, random_state=0)          # 2 = bicluster?
+    model = cluster.SpectralCoclustering(n_clusters=3, random_state=0)
     model.fit(X)
 
-    return X, model
+    return X, model, avg_df
+
+
+def calculate_bicluster_spectralbiclustering(training_set: TrainingSet, use_interpolation: bool = False) -> Tuple[np.ndarray, cluster.SpectralBiclustering, pd.DataFrame]:
+    avg_np, avg_df = training_set_to_data(training_set, use_interpolation=use_interpolation)
+    X = avg_np
+    model = cluster.SpectralBiclustering(n_clusters=3, random_state=0)
+    model.fit(X)
+
+    return X, model, avg_df
 
 
 def plot_biclustering(data_raw: np.ndarray, model: sklearn.base.BiclusterMixin, training_set: TrainingSet,
-                      save_to_file: bool = False):
+                      save_to_file: bool = False, used_interpolation: bool = False, feature_names: np.ndarray = None):
     """
     Sorts the data according to the biclusters and then plots the biclustering.
     :param data_raw: the unsorted data to be plotted
@@ -40,6 +62,13 @@ def plot_biclustering(data_raw: np.ndarray, model: sklearn.base.BiclusterMixin, 
     """
     data_row = data_raw[np.argsort(model.row_labels_)]
     data = data_row[:, np.argsort(model.column_labels_)]
+    feature_names = feature_names[np.argsort(model.column_labels_)]
+
+    clustering_type = ""  # for getting the biclustering method recorded
+    if isinstance(model, cluster.SpectralCoclustering):
+        clustering_type = "SpectralCoclustering"
+    elif isinstance(model, cluster.SpectralBiclustering):
+        clustering_type = "SpectralBiclustering"
 
     fig = plt.figure()
     fig.set_figheight(4)
@@ -47,27 +76,42 @@ def plot_biclustering(data_raw: np.ndarray, model: sklearn.base.BiclusterMixin, 
     ax = fig.add_subplot()
 
     ax.matshow(data, cmap=plt.cm.Blues)
-    ax.set_aspect(len(data[0]) / len(data))
+    ax.set_aspect(len(data[0]) / len(data))  # this makes the plot quadratic and prevents extreme stretching
     ax.set_xlabel('Features')
-    ax.set_xticks([i for i in range(len(data[0]))])
     ax.set_ylabel('Patients')
-    ax.set_title(f'"{training_set.name}" after biclustering; rearranged to show biclusters')
+
+    if feature_names is not None:
+        ax.set_xticklabels(labels=list(feature_names))
+        props = {"rotation": 90}
+        plt.setp(ax.get_xticklabels(), **props)
+    ax.set_xticks([i for i in range(len(data[0]))])
+
+    if not used_interpolation:
+        ax.set_title(f'"{training_set.name}" after biclustering; rearranged to show biclusters \n'
+                     f'({clustering_type})')
+    else:
+        ax.set_title(f'"{training_set.name}" after biclustering; rearranged to show biclusters \n'
+                     f'({clustering_type}, interpolation, quadratic)')
 
     fig.colorbar(matplotlib.cm.ScalarMappable(cmap=plt.cm.Blues,
                                               norm=matplotlib.colors.Normalize(vmin=min([min(v) for v in data]),
                                                                                vmax=max([max(v) for v in data]))),
                  ax=ax)
 
+    fig.tight_layout()
     if not save_to_file:
         plt.show()
     else:
-        plt.savefig(os.path.join(FIGURE_OUTPUT_FOLDER, f"bicluster-{training_set.name}"))
+        if used_interpolation:
+            plt.savefig(os.path.join(FIGURE_OUTPUT_FOLDER, f"bicluster-{clustering_type}-{training_set.name}-interpolated.png"))
+        else:
+            plt.savefig(os.path.join(FIGURE_OUTPUT_FOLDER, f"bicluster-{clustering_type}-{training_set.name}.png"))
     plt.close()
 
 
-def training_set_to_data(training_set: TrainingSet, use_interpolation: bool = False) -> np.ndarray:
+def training_set_to_data(training_set: TrainingSet, use_interpolation: bool = False) -> Tuple[np.ndarray, pd.DataFrame]:
     avg_df = training_set.get_average_df(fix_missing_values=True, use_interpolation=use_interpolation)
     avg_np = avg_df.transpose().to_numpy()
     avg_np.reshape(avg_np.shape[0], -1)  # does this have an effect?
 
-    return avg_np
+    return avg_np, avg_df
