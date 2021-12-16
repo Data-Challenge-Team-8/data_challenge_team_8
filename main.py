4bb8d826f54ef1d9d9c94caf66932a1d37960f41
Peery <peery.coding@gmail.com>
2021-12-16 14:20:05 +0100

    Reworked Cache System
    Changed the TrainingSet cache system to use multiple cache files.
    Integrated pacmap calculation into the TrainingSet class and its caching.
    Closes #3, #6.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import os
-import pickle
+import datetime
 
 from objects.training_set import TrainingSet
 from tools.subspace_clustering_analysis import implement_bicluster_spectralcoclustering, \
     implement_bicluster_spectralbiclustering
 from tools.visualization.interpolation_comparison import plot_most_interesting_interpolation_patients, \
     plot_data_with_and_without_interpolation
-from tools.pacmap_analysis import get_pacmap
 from tools.clustering_analysis import implement_DBSCAN, implement_k_means
 
 if __name__ == '__main__':
@@ -15,11 +13,12 @@ if __name__ == '__main__':
 
     # Task 2: Load Trainingset with interpolation (and caching)
     # set_a = TrainingSet(TrainingSet.PRESETS["Set A"][:61], name="Mini Set")
+    start_time = datetime.datetime.now()
     set_a = TrainingSet.get_training_set("Set A")
     # plot_most_interesting_interpolation_patients(set_a)
 
     # Task 3: Implement different clustering methods
-    pacmap_data, patient_ids = get_pacmap(set_a)
+    pacmap_data, patient_ids = set_a.get_pacmap()
     #implement_DBSCAN(set_a, pacmap_data, patient_ids)
     # implement_k_means(set_a, pacmap_data, patient_ids)
 
