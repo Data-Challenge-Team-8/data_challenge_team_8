8a65894941ee0fb660a26fab2a960b3b6876bc25
Tobias Ettling <T.ETTLING@BC-R.org>
2022-01-24 21:11:53 +0100

    Added the time series frontend. I Loaded a Trainingset, then the requested patient to access the Feature time series data. I interpolated the data for compleation.

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-from objects.training_set import TrainingSet
-from classifier.timeseries.time_series_forest import TimeSeriesForest
-from tools.visualization.time_series_comparison import plot_time_series_density, plot_two_time_series, \
-    plot_time_series_sepsis_background
+import sys
+import sys
+from streamlit import cli as stcli
+# from objects.training_set import TrainingSet
+# from classifier.timeseries.time_series_forest import TimeSeriesForest
+# from tools.visualization.time_series_comparison import plot_time_series_density, plot_two_time_series, \
+#     plot_time_series_sepsis_background
 
 if __name__ == '__main__':
-    set_a = TrainingSet.get_training_set("rnd Sample A")
+    sys.argv = ["streamlit", "run", ".\\web\\app.py"]
+    sys.exit(stcli.main())
+    # set_a = TrainingSet.get_training_set("rnd Sample A")
     # set_a = TrainingSet.get_training_set("Set A")
     # avg_df = set_a.get_average_df(use_interpolation=True, fix_missing_values=True)
     # sepsis_df = set_a.get_sepsis_label_df()
-    list_of_features = ["HR", "SepsisLabel"]
-    time_series = set_a.get_timeseries_df(use_interpolation=True, fix_missing_values=True,
-                                          limit_to_features=list_of_features)
-    set_a_sepsis_df = set_a.get_sepsis_label_df()
-
-    # plotting time_series for a single patient
-    plot_counter = 0
-    for patient_id in set_a.data:
-        if set_a.check_patient_has_sepsis(set_a_sepsis_df, patient_id):
-            temp_patient = set_a.get_patient_form_id(patient_id)
-            temp_patient.plot_features_time_series(list_of_features)
-            plot_counter += 1
-            if plot_counter > 100:
-                break
+    # list_of_features = ["HR", "SepsisLabel"]
+    # time_series = set_a.get_timeseries_df(use_interpolation=True, fix_missing_values=True,
+    #                                       limit_to_features=list_of_features)
+    # set_a_sepsis_df = set_a.get_sepsis_label_df()
+    #
+    # # plotting time_series for a single patient
+    # plot_counter = 0
+    # for patient_id in set_a.data:
+    #     if set_a.check_patient_has_sepsis(set_a_sepsis_df, patient_id):
+    #         temp_patient = set_a.get_patient_form_id(patient_id)
+    #         temp_patient.plot_features_time_series(list_of_features)
+    #         plot_counter += 1
+    #         if plot_counter > 100:
+    #             break
 
     # plotting the time_series data that was reduced to only 39 timesteps
     # temp_hr = None
@@ -50,14 +55,9 @@ if __name__ == '__main__':
     #     else:
     #         pass
 
-
-    # Implement TimeSeriesForest classification
-    # tsf = TimeSeriesForest(data_set=set_a, train_fraction=0.8, feature="HR")
-    # print("Starting setup ...")
-    # tsf.setup()
-    # print("Starting plotting ...")
-    # plot_time_series_density(tsf.train_data[0], label="HR", set_name=set_a.name+" (fixed, interpolated)")
-    # print("Start training & testing ...")
-    # tsf.train()
-    #
-    # tsf.display_confusion_matrix(plotting=True)
+    # Task 4: Implement different subspace clustering methods
+    # scikit_learn module biclustering, alternatives: houghnet, biclustlib
+    # print("Doing Biclustering ...")
+    # implement_bicluster_spectralcoclustering(set_a, use_interpolation=False)
+    # implement_bicluster_spectralbiclustering(set_a, use_interpolation=False)
+    # TODO convert result into cluster data for pacmap visualization?
