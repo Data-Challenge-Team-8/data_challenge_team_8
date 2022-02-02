d7538145525a4fa0affb4f630f2a1de9bb394588
Jakob V <vanek.jakob@googlemail.com>
2022-02-02 18:57:36 +0100

    finished k-means with near_miss clustering
    DBSCAN missing

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
     # Clustering after near miss
     implement_k_means_on_avg_df(training_set=set_a, avg_df=avg_df_near_miss,
-                                additional_options_title="interpolated, NearMiss(v2)", save_to_file=True)
+                                additional_options_title="interpolated_NearMiss(v2)", save_to_file=True)
 
     # todo. also implement DBSCAN
     # implement_DBSCAN_on_avg_df(training_set=set_a, avg_df=avg_df_near_miss, additional_options_title="interpolated, NearMiss(v2)", save_to_file=False)
