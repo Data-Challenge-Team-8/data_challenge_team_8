2bc49392841f5d0f18a3506d8029f34e47837622
Peery <peery.coding@gmail.com>
2022-02-04 22:00:54 +0100

    Added Distribution Plotting to Frontend
    Added 3 new plots to the "Exploratory Data Analysis" in the frontend that focus on the distribution of data across the values. Each is a histogram of all values recorded, the patient averages and patient deviations with indicators of the average and the standard deviations of the displayed distribution.

--- web/categories/exploratory_data_analysis.py
+++ web/categories/exploratory_data_analysis.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from matplotlib import pyplot as plt
+import pandas as pd
 
 from objects.training_set import TrainingSet
 from tools.analyse_tool import CompleteAnalysis
@@ -7,10 +8,12 @@ from objects.patient import Patient
 
 
 class ExploratoryDataAnalysis:
-
     TOOL_SELECTION = {"avg": "Min, Max, Average, Variance",
                       "missing": "Missing Values",
-                      #"subgroup": "Subgroups",
+                      "all_distribution": "Distribution",
+                      "avg_distribution": "Distribution (Avg)",
+                      "dev_distribution": "Distribution (deviation)",
+                      # "subgroup": "Subgroups",
                       }
 
     def __init__(self):
@@ -41,8 +44,8 @@ class ExploratoryDataAnalysis:
                   " If the dataset has not been loaded previously, the background analysis might take up to 3 minutes."
         st.markdown(info_p1)
 
-    @staticmethod
-    def plot_selected_analysis(analysis_obj: CompleteAnalysis, selected_label, selected_tool, selected_set, col2, col3):
+    def plot_selected_analysis(self, analysis_obj: CompleteAnalysis, selected_label, selected_tool, selected_set, col2,
+                               col3):
         if selected_tool == ExploratoryDataAnalysis.TOOL_SELECTION["avg"]:
             min_value = analysis_obj.min_for_label[selected_label][1]
             max_value = analysis_obj.max_for_label[selected_label][1]
@@ -64,5 +67,55 @@ class ExploratoryDataAnalysis:
             col2.pyplot(fig, transparent=True)  # transparent looks much better imo
             col3.metric("Missing (red)", str(round((missing_vals_rel * 100))) + "%")
             col3.metric("Not Missing (green)", str(round(((1 - missing_vals_rel) * 100))) + "%")
+        elif selected_tool == ExploratoryDataAnalysis.TOOL_SELECTION["avg_distribution"]:
+            fig = self.__plot_distribution(analysis_obj, selected_label, selected_set, method="avg")
+            col2.pyplot(fig)
+        elif selected_tool == ExploratoryDataAnalysis.TOOL_SELECTION["all_distribution"]:
+            fig = self.__plot_distribution(analysis_obj, selected_label, selected_set, method="all")
+            col2.pyplot(fig)
+        elif selected_tool == ExploratoryDataAnalysis.TOOL_SELECTION["dev_distribution"]:
+            fig = self.__plot_distribution(analysis_obj, selected_label, selected_set, method="dev")
+            col2.pyplot(fig)
         else:
             st.write("Feature not implemented yet.")
+
+    def __plot_distribution(self, analysis_obj: CompleteAnalysis, selected_label: str, selected_set: str,
+                            method: str = "avg") -> plt.Figure:
+        fig, ax = plt.subplots()
+
+        data_series = pd.Series()
+        if method == "avg":
+            data_series = TrainingSet.get_training_set(selected_set).get_average_df().loc[selected_label]
+        elif method == "all":
+            for patient in TrainingSet.get_training_set(selected_set).data.values():
+                data_series = data_series.append(patient.data[selected_label], ignore_index=True)
+        elif method == "dev":
+            for patient in TrainingSet.get_training_set(selected_set).data.values():
+                data_series = data_series.append(pd.Series(patient.data[selected_label].var() ** .5), ignore_index=True)
+        min_value = min(data_series)
+        max_value = max(data_series)
+        avg_value = data_series.mean()
+        variance = data_series.var()
+        bins = int(max_value - min_value * 0.8)
+        sepsis_df = TrainingSet.get_training_set(selected_set).get_sepsis_label_df().astype(bool)
+        sepsis_df.insert(1, selected_label, data_series, allow_duplicates=True)
+
+        ax.hist(data_series.tolist(), bins=bins, label=selected_label)
+        ax.hist(sepsis_df[sepsis_df.loc[:, "SepsisLabel"]].loc[:, selected_label], bins=bins, label="Sepsis")
+
+        ax.vlines([avg_value + variance ** .5, avg_value - variance ** .5], ymin=ax.get_ylim()[0],
+                  ymax=ax.get_ylim()[1],
+                  label=f"std. deviation ({selected_label})", color='r', alpha=0.5, linestyles="dashed")
+        ax.vlines([avg_value], ymin=ax.get_ylim()[0], ymax=ax.get_ylim()[1], label=f"average ({selected_label})",
+                  color='g', alpha=0.9, linestyles='dashdot')
+        if method == "avg":
+            ax.set_title(f"Distribution of Average {selected_label} across {selected_set}")
+        elif method == "all":
+            ax.set_title(f"Distribution of {selected_label} values across {selected_set}")
+        elif method == "dev":
+            ax.set_title(f"Distribution of {selected_label} deviations across {selected_set}")
+        ax.set_xlabel("value")
+        ax.set_ylabel("count")
+        ax.legend()
+
+        return fig
