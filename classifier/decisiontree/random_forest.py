3e635e51952f837a7bc751b566a065994b54781f
Peery <peery.coding@gmail.com>
2022-02-05 14:19:09 +0100

    Implemented ROC & AUC score to RandomForest
    Added automatic plotting and scoring via ROC and AUC

--- classifier/decisiontree/random_forest.py
+++ classifier/decisiontree/random_forest.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, classification_report
-
+from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, plot_roc_curve
 from classifier.classifier import Classifier
 
 
@@ -11,7 +10,7 @@ class RandomForest(Classifier):
     def __init__(self, max_depth: int = 5, tree_count: int = 100, use_bootstrapping: bool = True):
         super().__init__()
         self.__model = RandomForestClassifier(n_estimators=tree_count, max_depth=max_depth, bootstrap=use_bootstrapping,
-                                              random_state=1337)            # Jakob: added random state here
+                                              random_state=1337)
 
     def train(self, x_data, y_data):
         self.__model.fit(x_data, y_data)
@@ -36,7 +35,15 @@ class RandomForest(Classifier):
     def get_confusion_matrix_df(self, y_data, y_predicted) -> pd.DataFrame:
         cm = confusion_matrix(y_data, y_predicted)
         cm_df = pd.DataFrame({
-            "predicts_false": {"is_false": cm[0][0], "is_true": cm[1][0]},          # jakob: changed fields buttom left and top right
+            "predicts_false": {"is_false": cm[0][0], "is_true": cm[1][0]},
             "predicts_true": {"is_false": cm[0][1], "is_true": cm[1][1]}
         })
         return cm_df
+
+    def plot_roc_curve(self, X_test, y_test, title: str = ""):
+        return plot_roc_curve(self.__model, X_test, y_test, name=title)
+
+    def get_roc_auc_score(self, X_test, y_test):
+        roc = roc_auc_score(y_test, self.__model.predict_proba(X_test)[:, 1])
+        return roc
+
