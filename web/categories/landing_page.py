e600af0b00e0355fb61db90ec62750dfb07183c8
aline <aline230198@gmail.com>
2022-02-06 00:56:51 +0100

    Display_table_function with new format

--- web/categories/landing_page.py
+++ web/categories/landing_page.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import datetime
 import pandas as pd
+import numpy as np
 
 from objects.training_set import TrainingSet
 from tools.analyse_tool import CompleteAnalysis as ca, CompleteAnalysis
 from PIL import Image
+from decimal import Decimal
 
 
-def display_feature_graphic():
+def display_feature_graphic(col1):
     feature_graphic = Image.open(r'./data/feature_graphic.jpg')
     st.image(feature_graphic, caption='Descriptions for each feature from the underlying PhysioNet paper')
 
@@ -17,24 +19,35 @@ def display_table(selected_set_name: str, col2):
     st.markdown(info_p3)
     temp_ca, cache_file_name = CompleteAnalysis.get_analysis(selected_label="fake_label", selected_tool='fake_tool',
                                                              selected_set=selected_set_name)
-    general_info = {'Hospital System': ['Number of patients', 'Number of septic patients', 'Sepsis prevalence',
-                                        'Number of entries', 'Number of NaNs', 'Relative number of NaNs',
+
+    general_info = {'Hospital System': ['Number of patients', 'Number of septic patients', 'Sepsis prevalence in %',
+                                        'Number of entries', 'Number of NaNs', 'NaN prevalence in %',
                                         'Total hours recorded', 'Average hospital stay duration (hours)'],
-                    selected_set_name: [int(temp_ca.total_patients),
-                                        int(temp_ca.sepsis_patients_count),
-                                        temp_ca.rel_sepsis_amount,
-                                        int(temp_ca.data_amount),
-                                        int(temp_ca.total_nan),
-                                        temp_ca.rel_nan_total,
-                                        int(temp_ca.total_time_measured),
-                                        temp_ca.avg_data_duration_total]
-                    }
+                     selected_set_name: [(int(temp_ca.total_patients)),
+                                         int(temp_ca.sepsis_patients_count),
+                                         round((temp_ca.rel_sepsis_amount*100),2),
+                                         int(temp_ca.data_amount),
+                                         int(temp_ca.total_nan),
+                                         round((temp_ca.rel_nan_total)*100,2),
+                                         int(temp_ca.total_time_measured),
+                                         round(temp_ca.avg_data_duration_total,2)]
+                     }
+
     df_general_info = pd.DataFrame(general_info)
 
-    df_general_info.style.format(na_rep='MISSING', thousands=".")           # todo: formatierung vom Dataframe kann man schöner machen, vor allem die Nullstellen von ints
+    #Lösung1
+    df_general_info[selected_set_name] = df_general_info[selected_set_name].astype(str)
+    df_general_info[selected_set_name] = df_general_info[selected_set_name].str.replace('.0', ' ', regex = False)
 
+    df_general_info = df_general_info.style.format(na_rep='MISSING')
     col2.dataframe(df_general_info)
-    # col2.table(df_general_info)           # sieht doof aus
+
+    #Lösung 2(funktioniert so nicht aber wäre schöner mit Trennzeichen implementierbar)
+    #df_general_info[selected_set_name]=df_general_info[selected_set_name].astype(float).round(2)
+    #df_general_info[selected_set_name]= [round(x) if x == round(x) else "{0:.2f}".format(x) for x in df_general_info[selected_set_name]]
+    #col2.dataframe(df_general_info)
+
+    #col2.table(general_info)           # sieht doof aus
 
 
 def write_info_text(col1):  # maybe: if you have any questions or contact: our emails?
@@ -48,11 +61,10 @@ def write_info_text(col1):  # maybe: if you have any questions or contact: our e
     col1.markdown(info_p2)
 
 
-def write_info_text_2():
+def write_info_text_2(col1):
     info_p4 = "Below the features collected in the dataset and their respective descriptions are displayed:"
-    st.markdown(info_p4)
 
-def start_loading(selected_set_list, selected_label_list):
+def start_loading(selected_set_list, selected_label_list, col1):
     total_start_time = datetime.datetime.now()
     print("Loading started at time:", str(datetime.datetime.now()).replace(" ", "_").replace(":", "-"))
     for unique_set in selected_set_list:
@@ -76,17 +88,17 @@ class LandingPage:
               "SepsisLabel"]
 
     def __init__(self):
-        col1, col2, col3, col4 = st.columns((0.5, 2, 1, 0.5))           # hiermit kann man "ränder" erstellen und test in columns machen
+        col1, col2, col3, col4 = st.columns((0.3, 2, 1.8, 0.3))           # hiermit kann man "ränder" erstellen und test in columns machen
         col2.markdown("<h2 style='text-align: left; color: black;'>Project Description</h2>", unsafe_allow_html=True)
         write_info_text(col2)
         selected_set_name = self.create_selector(col3)
         display_table(selected_set_name, col3)
 
-        write_info_text_2()                 # todo: vlt sollten die ganzen nachfolgenden Elemente auf der Seite auch innerhalb von columns angeordnet sein. Sieht besser aus
-        display_feature_graphic()
-        self.display_load_data_upfront()        # todo: vlt sollte dieses "Cache laden" ganz oben sein, und auch anders heißen?
+        write_info_text_2(col2)                 # todo: vlt sollten die ganzen nachfolgenden Elemente auf der Seite auch innerhalb von columns angeordnet sein. Sieht besser aus
+        display_feature_graphic(col2)
+        self.display_load_data_upfront(col2)        # todo: vlt sollte dieses "Cache laden" ganz oben sein, und auch anders heißen?
 
-    def display_load_data_upfront(self):
+    def display_load_data_upfront(self,col2):
         multiselect_label_list = ['0_Load all labels (long waiting time!)']
         for label in self.LABELS:
             multiselect_label_list.append(label)
